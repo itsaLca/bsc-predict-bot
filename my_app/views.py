c0574929edbb20b20b413c7514edaf28c4aaab89
Lorenzo V <33633818+lorenzovella@users.noreply.github.com>
2022-01-07 17:11:18 -0300

    .

--- my_app/views.py
+++ my_app/views.py
@@ -1,19 +1,12 @@
 from django.shortcuts import render
 from datetime import datetime
 from django.views.generic.base import TemplateView
-import ../predict/main
+from ../predict/main import Robot
 
 def index(request):
     data =  data=datetime.today().strftime('%d/%m/%Y')
     calcData = "oi"
     return render(request,'index.html',{'dia':data})
 
-def runCommand(request, strategy, size):
-    cmd = f"python main.py --strategy {stategy} --size {size}"
-    process = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-    while True:
-        nextline = process.stdout.readline()
-        if nextline == '' and process.poll() is not None:
-            break
-        sys.stdout.write(nextline)
-        sys.stdout.flush()
+def runCommand(request, strategy, ammount):
+    robotInstace = Robot(strategy, ammount)
