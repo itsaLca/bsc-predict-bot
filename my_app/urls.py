c13a52e4b30a08df41a62fed94abfc4d256fc4a0
Lorenzo V <33633818+lorenzovella@users.noreply.github.com>
2022-01-07 18:18:59 -0300

    .

--- my_app/urls.py
+++ my_app/urls.py
@@ -4,7 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name="index"),
-    path('start/?<str:strategy>;<str:ammount>', views.startRobot, name="start"),
-    path('stop', views.stopRobot, name="stop"),
-
+    path('/start/<str:strategy>/<str:ammount>', views.startRobot, name="start"),
+    path('/stop', views.stopRobot, name="stop"),
 ]
