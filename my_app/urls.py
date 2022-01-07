db8072fdd905d14efc0551f05259f70389556cf5
Lorenzo V <33633818+lorenzovella@users.noreply.github.com>
2022-01-07 16:24:57 -0300

    .

--- my_app/urls.py
+++ my_app/urls.py
@@ -4,6 +4,4 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name="index"),
-    path('metatrader', views.metatrader, name="index"),
-    path('calculo/<str:data>', views.calculo, name="view-calculo"),
 ]
--- my_app/views.py
+++ my_app/views.py
@@ -1,17 +1,8 @@
 from django.shortcuts import render
-from . import calculo1
-from . import mt5
 from datetime import datetime
 from django.views.generic.base import TemplateView
 
-def calculo(request, data):
-    calcData = calculo1.calc(data.replace('-','/'))
-    return render(request,'calculo1.html',{'dia':data,'pontos':calcData['pontos'],'calculo1':calcData['calculo1']})
-
 def index(request):
     data =  data=datetime.today().strftime('%d/%m/%Y')
-    calcData = calculo1.calc(data)
-    return render(request,'index.html',{'dia':data,'pontos':calcData['pontos'],'calculo1':calcData['calculo1']})
-
-def metatrader(request):
-    return render(request,'metatrader.html',{'res': mt5.calc()})
+    calcData = "oi"
+    return render(request,'index.html',{'dia':data,'pontos':calcData,'calculo1':calcData})
