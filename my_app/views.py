75c13978b9bfbb0ce14201749418c367a4bf1699
Lorenzo V <33633818+lorenzovella@users.noreply.github.com>
2022-01-07 17:09:53 -0300

    .

--- my_app/views.py
+++ my_app/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render
 from datetime import datetime
 from django.views.generic.base import TemplateView
-import subprocess
+import ../predict/main
+
 def index(request):
     data =  data=datetime.today().strftime('%d/%m/%Y')
     calcData = "oi"
-    return render(request,'index.html',{'dia':data,'pontos':calcData,'calculo1':calcData})
+    return render(request,'index.html',{'dia':data})
 
 def runCommand(request, strategy, size):
     cmd = f"python main.py --strategy {stategy} --size {size}"
--- predict/main.py
+++ predict/main.py
@@ -3,62 +3,22 @@ from dotenv import load_dotenv
 import os
 import strategies
 
-if __name__ == "__main__":
-  load_dotenv()
-  
-  parser = argparse.ArgumentParser(description='Trading bot for Pancake Swap Prediction Markets or BSC-Predict')
-  parser.add_argument(
-    '--strategy',
-    dest='strategy',
-    action='store',
-    required=True,
-    help="Strategies are file names defined in the strategies/ folder (e.g. SimpleBot)"
-  )
-  parser.add_argument(
-    '--size',
-    dest='size_eth',
-    action='store',
-    type=float,
-    required=True,
-    help="Amount in eth to bet",
-  )
-  parser.add_argument(
-    '--min',
-    dest='min_balance_eth',
-    action='store',
-    type=float,
-    required=False,
-    default=0,
-    help="The minimum balance of your account before the bot stops playing",
-  )
-  parser.add_argument(
-    '--dry',
-    dest='dry',
-    action='store_true',
-    required=False,
-    default=False,
-    help="Dry run",
-  )
 
+class Robot():
+  def __init__(self, strategy, ammount):
+    SECRET_KEY = os.getenv('secret')
+    ACCOUNT = os.getenv('account')
+    self.stategy = stategy
+    self.ammount = ammount
+    if (SECRET_KEY is None and not args.dry):
+      raise Exception("SECRET_KEY is not defined in .env")
+    elif (ACCOUNT is None and not args.dry):
+      raise Exception("ACCOUNT is not defined in .env")  
+    __import__(f'strategies.{strategy}', locals(), globals())
+    import strategies
 
-  SECRET_KEY = os.getenv('secret')
-  ACCOUNT = os.getenv('account')
-
-  args = parser.parse_args()
-  strategy = args.strategy
-  bet_size_eth = args.size_eth
-  min_balance_eth = max(0, args.min_balance_eth)
-
-  if (SECRET_KEY is None and not args.dry):
-    raise Exception("SECRET_KEY is not defined in .env")
-  elif (ACCOUNT is None and not args.dry):
-    raise Exception("ACCOUNT is not defined in .env")
-
-  
-  __import__(f'strategies.{strategy}', locals(), globals())
-  import strategies
-
-  bot = getattr(strategies, strategy)
-  bot = bot.Bot(dry=args.dry, account=ACCOUNT, secret_key=SECRET_KEY, bet_size_eth=bet_size_eth, min_balance_eth=min_balance_eth)
-  bot.run()
+  def start():
+    bot = getattr(trategies, strategy)
+    bot = bot.Bot(dry=args.dry, account=ACCOUNT, secret_key=SECRET_KEY, bet_size_eth=bet_size_eth, min_balance_eth=min_balance_eth)
+    bot.run()
 
