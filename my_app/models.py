7a4abba643e79f2262fa6ca2f08d49ba793223dc
Lorenzo V <33633818+lorenzovella@users.noreply.github.com>
2022-01-09 20:36:22 -0300

    .

--- my_app/models.py
+++ my_app/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+
+class robots(models.Model):
+    nome = models.CharField(max_length=200)
+    tipo = models.CharField(max_length=300)
+    last_updated = models.DateTimeField(auto_now_add=True, editable=False)
+    created = models.DateTimeField(auto_now_add=True, editable=False)
+
+    class Meta:
+        pass
+
+    def __str__(self):
+        return str(self.nome)
+
+    def status(self):
+        return [self.nome, self.tipo, self.created]
