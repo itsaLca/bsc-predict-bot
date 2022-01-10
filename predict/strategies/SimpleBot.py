08a8eb9bd6109203026b074dc1fe02813cb9b028
Lorenzo Vella <lorenzo.vella@csghq.com>
2022-01-10 18:01:18 -0300

    .

--- predict/strategies/SimpleBot.py
+++ predict/strategies/SimpleBot.py
@@ -13,7 +13,7 @@ from telegramHandler import sendMessage
 # NOTE: Class must be named Bot
 class Bot(BaseBot):
   def get_bet(self, upcoming: Round) -> Optional[Bet]:
-    # YYYY-MM-DD 
+    # YYYY-MM-DD
     # This function returns either a Bet or None based on the upcoming round.
     # If it returns a Bet, then that bet will be made
     # upcoming is the upcoming round that you're betting on
@@ -22,24 +22,24 @@ class Bot(BaseBot):
     timestamp = time.time()
     if upcoming.lockTimestamp - timestamp > 30:
       return None
-    
+
     # you also have access to `self.history` which is the entire history of the games
-    # At any time, there is an upcoming round that you can bet on, a `live` round that has not yet closed and the history  
+    # At any time, there is an upcoming round that you can bet on, a `live` round that has not yet closed and the history
     # here we filter on completed rounds or rounds that closed
     completed = [r for r in self.history if r.oracleCalled]
     last_winner = completed[-1]
 
-    sendMessage(f"Last Winner: {last_winner.winner}")
-    logging.info(f"Last Winner: {last_winner.winner}")
 
     binance = ccxt.binance()
-    bars = binance.fetch_ohlcv('BTC/USDT','5m',limit=133)
+    bars = self.binance.fetch_ohlcv('BNB/USDT','5m',limit=133)
     df = pd.DataFrame(bars, columns=['date', 'open', 'high', 'low', 'close','vol'])
     dfC = df['close']
     sma50 = df['close'].rolling(50).mean()
-    logging.info(dfC.iloc(-1), sma50.iloc(-1))
-    if (dfC.iloc(-1) > sma50.iloc(-1)):
+
+    sendMessage(f"Last Winner: {last_winner.winner}")
+    logging.info(f"Last Winner: {last_winner.winner}")
+
+    if (dfC.iloc[-1] > sma50.iloc[-1]):
       return Bet(direction=Direction.BULL, amount_eth=self.bet_size_eth, epoch=upcoming.epoch)
-    elif (dfC.iloc(-1) < sma50.iloc(-1)):
+    elif (dfC.iloc[-1] < sma50.iloc[-1]):
       return Bet(direction=Direction.BEAR, amount_eth=self.bet_size_eth, epoch=upcoming.epoch)
-  
