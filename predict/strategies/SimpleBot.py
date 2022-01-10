7099f4c3cdc3abe4dd1ef8967edc20dd2442b389
Lorenzo V <33633818+lorenzovella@users.noreply.github.com>
2022-01-09 21:23:51 -0300

    .

--- predict/strategies/SimpleBot.py
+++ predict/strategies/SimpleBot.py
@@ -38,8 +38,8 @@ class Bot(BaseBot):
     dfC = df['close']
     sma50 = df['close'].rolling(50).mean()
     logging.info(dfC.iget(-1), sma50.iget(-1))
-    if (dfC.iget(-1) > sma50.iloc(-1)):
+    if (dfC.iloc(-1) > sma50.iloc(-1)):
       return Bet(direction=Direction.BULL, amount_eth=self.bet_size_eth, epoch=upcoming.epoch)
-    elif (dfC.iloc(-1) < sma50.iget(-1)):
+    elif (dfC.iloc(-1) < sma50.iloc(-1)):
       return Bet(direction=Direction.BEAR, amount_eth=self.bet_size_eth, epoch=upcoming.epoch)
   
