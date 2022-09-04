355e6e1e672d1feb563676034a7809186d845d46
Dohn Joe <aLca@ZeroDay.vip>
2022-09-04 16:01:18 +0200

    fixed

--- django_heroku/settings.py
+++ django_heroku/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "ee37068e-9315-458a-bd70-23b794e93500" #os.environ.get("djangosecret")
+SECRET_KEY = os.environ.get("djangosecret")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1','bsc-predict.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1','bscpjl.herokuapp.com']
 
 
 # Application definition
